3cea10fd80ef82797226d9a9fecf7d2b66e2382d
yogeshsunder <sunderyogesh@gmail.com>
2024-07-30 10:56:21 +0530

    Making Nav and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './componenets/Nav';
+import Footer from './componenets/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>E-Dashboard</h1>
+      <BrowserRouter>
+      <Nav />
+      
+      <Routes>
+        <Route path='/' element={<h1>Product Listing Component</h1>}/>
+        <Route path='/add' element={<h1>Add Product Listing Component</h1>}/>
+        <Route path='/update' element={<h1>Udate Product Listing Component</h1>}/>
+        <Route path='/logout' element={<h1>Logout Component</h1>}/>
+        <Route path='/profile' element={<h1>Profile Component</h1>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }

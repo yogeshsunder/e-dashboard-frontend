5c325c9e27d84cdc47b9c38f70de7c9d58ed174b
yogeshsunder <sunderyogesh@gmail.com>
2024-10-16 22:44:21 +0530

    Creating Product list UI

--- src/componenets/ProductList.js
+++ src/componenets/ProductList.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 const ProductList=()=>{
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        getProducts();
+    }, [])
+
+    const getProducts=async ()=>{
+        let result = await fetch('http://localhost:5000/products');
+        result = await result.json();
+        setProducts(result);
+    }
+    
+    const deleteProduct = async (id)=>{
+        let result = await fetch(`http://localhost:5000/product/${id}`,{
+            method:"Delete"
+        });
+        result = await result.json();
+        if(result)
+        {
+            getProducts();
+        }
+      };
+
+
     return(
-        <div>
-            <h1>Product listing showing here</h1>
+        <div className="product-list">
+            <h1>Product listing showing here:</h1>
+            <ul className='product-list-heading'>
+                <li>S. No.</li>
+                <li>Name</li>
+                <li>Price</li>
+                <li>Category</li>
+                <li>Operation</li>
+            </ul>
+            {
+                products.map((item, index)=>
+                <ul key={item._id}>
+                    <li>{index+1}</li>
+                    <li>{item.name}</li>
+                    <li>{item.price}</li>
+                    <li>{item.category}</li>
+                    <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
+                </ul>
+            )
+                
+            }
+            
         </div>
         
     )
